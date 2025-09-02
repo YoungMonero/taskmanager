47644a5d9332041ff2b5a0e85ef9e878793f779d
youngmonero <yungmonero29@gmail.com>
2025-09-02 13:44:26 +0100

    initial commit

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -5,6 +5,12 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,  // Set to console.log for debugging
 });
 
